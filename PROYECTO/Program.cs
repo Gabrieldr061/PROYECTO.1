712a598c9963bd0040d4e1d873c8fd58c93459e1
Angel Araujo <angel1.araujo2004@gmail.com>
2025-07-03 10:33:33 -0600

    Hice una nueva Clasexd

--- PROYECTO/Program.cs
+++ PROYECTO/Program.cs
@@ -1,12 +1,36 @@
 ﻿namespace PROYECTO
 {
-    internal class Program
+    public class Clasexd
     {
-        static void Main(string[] args)
+        //Atributos
+        public int id;
+        public string name; //si queremos cambiar una variable en todas cntrl + R y clic
+        public float peso;
+        public bool estado;
+
+        //Metodos
+        public void Presentarse()
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Pide la alberca");
-            Console.WriteLine("Prende el server");
+            Console.WriteLine("Hola mi nombre es {0}", name);
+        }
+
+        public void Brincar()
+        {
+            Console.WriteLine("Mirame estoy brincando");
+        }
+
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Clasexd ente = new Clasexd();
+                ente.id = 1;
+                ente.name = "Angel";
+                ente.peso = 12.25f;
+                ente.estado = true;
+
+                ente.Presentarse();
+            }
         }
     }
 }
