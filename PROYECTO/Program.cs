b81f7ac303ad0a5fdd08597ef26267236eebdf4b
Emilio Espinosa <emilioram2701@outlook.com>
2025-07-03 10:32:28 -0600

    Clase

--- PROYECTO/Program.cs
+++ PROYECTO/Program.cs
@@ -2,11 +2,38 @@
 {
     internal class Program
     {
+        public class Ente
+        {
+            public int id;
+            public string name;
+            public float peso;
+            public float altura;
+
+            public void Presentarse()
+            {
+
+            }
+
+            public void Presentarse(string nombre)   //parametro dentro del parentesis
+            {
+                Console.WriteLine(" Hola soy {0} y mi id es {1} mi peso es {2}kg y mi altura es {3} cm", name, id, peso, altura);  //argumentos y atributos no es lo mismo
+            }
+
+            public void Presentarse(string nombre, int id)
+            {
+                Console.WriteLine("Mi nombre es {0} y mi id es {1}", nombre, id);
+            }
+
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("Hola bebé");
-            
+            Ente ente2 = new Ente();
+            ente2.id = 50;
+            ente2.altura = 1.90f;
+            ente2.peso = 75.5f;
+            ente2.name = "Emilio";
+            ente2.Presentarse("Emilio");
+
         }
     }
 }
