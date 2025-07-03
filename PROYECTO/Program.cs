cc4f4c74d669da1e9df8bbf5a660b90d7a1fa9a4
Emilio Espinosa <emilioram2701@outlook.com>
2025-07-03 10:11:22 -0600

    linea 8

--- PROYECTO/Program.cs
+++ PROYECTO/Program.cs
@@ -5,7 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("Putos");
+            Console.WriteLine("Hola bebé");
+            
         }
     }
 }
